3337bfa358c5c17b9d1347a2fa78089e04ef3302
Federico Lo Dolce <flodolce92@gmail.com>
2024-02-28 03:51:09 +0100

    mo funziona meglio pare

--- src/ft_split.c
+++ src/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 19:50:44 by flo-dolc          #+#    #+#             */
-/*   Updated: 2024/02/28 03:21:32 by flo-dolc         ###   ########.fr       */
+/*   Updated: 2024/02/28 03:50:46 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,12 @@ static int	count_words(const char *str, char c)
 	i = 0;
 	while (str[i])
 	{
+		if (str[i] == '\'' || str[i] == '\"')
+		{
+			i++;
+			while (str[i] != '\'' && str[i] != '\"' && str[i])
+				i++;
+		}
 		if ((str[i] != c && str[i])
 			&& (str[i + 1] == c || str[i + 1] == '\0'))
 			count++;
